10590fd90acafdbbfc4786d93845446a7826bed1
AsrMzr <amsyar96@hotmail.com>
2020-01-12 00:14:02 +0800

    Update_MasterTemplate,Navbar,Index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Detect the current session
 session_start();
 
-$MainContent = "<img src='Images/welcome2mamaya.jpg'  
+$MainContent = "<img src='Images/baby.jpg'  
                      class='img-fluid' 
                      style='display:block; margin:auto;'/>";
 
--- navbar.php
+++ navbar.php
@@ -2,8 +2,8 @@
 //Display guest welcome message, Login and Registration links
 //when shopper has yet to login,
 $content1 = "Welcome Guest<br />";
-$content2 = "<li class='nav-item'>
-		     <a class='nav-link' href='register.php'>Sign Up</a></li>
+$content2 = "
+		     
 			 <li class='nav-item'>
 		     <a class='nav-link' href='login.php'>Login</a></li>";
 
@@ -11,7 +11,7 @@ if(isset($_SESSION["ShopperName"])) {
 	//To Do 1 (Practical 2) - 
     //Display a greeting message, Change Password and logout links 
     //after shopper has logged in.
-	$content1="Welcome <b>$_SESSION[ShopperName]</b>";
+	$content1="<b>Welcome</b> <b>$_SESSION[ShopperName]</b>";
 	$content2="<li class ='nav-item'> <a class='nav-link' href='changePassword.php'>Change Password</a></li><li class ='nav-item'><a class = 'nav-link' href = 'logout.php'>Logout</a></li>";
 	
 	//To Do 2 (Practical 4) - 
@@ -25,33 +25,34 @@ if(isset($_SESSION["ShopperName"])) {
 ?>
 <!-- To Do 3 (Practical 1) - 
      Display a navbar which is visible before or after collapsing -->
-	 <nav class ="navbar navbar-expand-md navbar-dark bg-dark">
+	 <nav class ="navbar navbar-expand-md navbar-light bg-light">
 	 <!--Dynamic Text Display-->
 	 <span class ="navbar-text ml-md-2"
-	 style="color:#F7BE81;max-width:80%;">
+	 style="color:#333300;max-width:80%;">
 	 <?php echo $content1;?>
 	 </span>
 	 <!-- Toggler /Collapsibe Button -->
-	 <button class ="navbar-toggler" type ="button" data-toggle = "collapse" data-target = #collapsibleNavbar">
+	 
+	 <button class ="navbar-toggler" type ="button" data-toggle = "" data-target = #collapsibleNavbar">
 	 <span class ="navbar-toggler-icon"></span>
 	 </button>
 	 </nav>
 
 <!-- To Do 4 (Practical 1) - 
      Define a collapsible navbar -->
-	 <nav class ="navbar navbar-expand-md navbar-dark bg-dark">
+	 <nav class ="navbar navbar-expand-md navbar-light bg-light">
 	 <!-- Collapsible part of navbar -->
 	 <div class = "collapse navbar-collapse" id="collapsibleNavbar">
 	 <!-- Left-justified menu items-->
-	 <ul class = "navbar-nav mr-auto">
+	 <ul class = "navbar-nav mr-auto style">
 	 <li class="nav-item">
-	 <a class ="nav-link" href="category.php">Product Categories</a>
+	 <a class ="nav-link  btn-outline-info"  href="Membership_Registration.php">Membership Registration</a>
 	 </li>
 	 <li class="nav-item">
-	 <a class ="nav-link" href="search.php">Product Search</a>
+	 <a class ="nav-link  btn-outline-info"  href="Category.php">Product Catalogue</a>
 	 </li>
 	 <li class="nav-item">
-	 <a class ="nav-link" href="shoppingCart.php">Shopping Cart</a>
+	 <a class ="nav-link  btn-outline-info"  href="shoppingCart.php">Shopping Cart</a>
 	 </li>
 	 </ul>
 	 <!-- Right-justifed menu items-->
